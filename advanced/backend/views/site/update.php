60288f3e6d03ddce18fd5a6963a1e1a9765d25a1
leng1110 <lengjiafeng>
2017-12-04 19:10:16 +0800

    delete

--- advanced/backend/views/site/update.php
+++
@@ -1,9 +0,0 @@
-<center>
-	<form action="/site/updateto" method="post" accept-charset="utf-8">
-		<table>
-			<tr>姓名：<input type="text" name="goods_name" value="<?= $res['goods_name']?>" /><input type="hidden" value="<?= $res['id']?>" name="id" /></tr>
-			<tr>性别：<input type="text" name="goods_price" value="<?= $res['goods_price']?>" /></tr>
-			<tr><input type="submit" value="修改" /></tr>
-		</table>
-	</form>
-</center>
--- advanced/frontend/views/site/add.php
+++
@@ -1,9 +0,0 @@
-<center>
-	<form action="/site/addt" method="post">
-	<table border="1">
-		<tr>姓名：<input type="text" name="goods_name" /></tr>
-		<tr>性别：<input type="text" name="goods_price" /></tr>
-		<tr><input type="submit" value="提交"></tr>		
-	</table>
-	</form>
-</center>
